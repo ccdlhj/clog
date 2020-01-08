43bd1e89a20f81c49f4c4b7b49f01c4a8e9be38e
gupeng <gupeng@sinorail.com>
2020-01-08 19:07:18 +0800

    feature(Clog): add clog api

--- cloud_log/exceptions.py
+++ cloud_log/exceptions.py
@@ -32,15 +32,13 @@ CODE = enum(
 )
 
 
-class PortalAuthException(APIException):
-    """Generic error class to identify and catch our own errors."""
-    code = CODE.SERVER_ERROR_CODE
+class InternalServerError(APIException):
     status_code = 500
     title = 'Internal Server Error'
-    message_format = _('Internal Server Error')
+    message_format = _('Internal Server Error {message}')
 
-    def __init__(self, exc=None, message=None, status_code=None, **kwargs):
-        self.message = message or self.message_format
+    def __init__(self, message=None, exc=None, status_code=None, **kwargs):
+        self.message = message
         self.status_code = status_code or self.status_code
         self.__dict__.update(kwargs)
         self._parse(exc)
@@ -48,130 +46,14 @@ class PortalAuthException(APIException):
 
     def _parse(self, exc):
         if exc:
-            self.message = getattr(exc, "message", None) or self.message
-            self.message_format = getattr(exc, "message_format", None) or self.message_format
-            self.status_code = getattr(exc, "status_code", None) or self.status_code
+            self.message_format = \
+                getattr(exc, "message_format", None) or self.message_format
+            message = getattr(exc, "message", None) or self.message or ''
+            self.message = self.message_format.format(message=message)
+            self.status_code = \
+                getattr(exc, "status_code", None) or self.status_code
             self.title = getattr(exc, "title", None) or self.title
-            self.code = getattr(exc, "code", None) or self.code
             self.__dict__.update(exc.__dict__)
 
     def __str__(self):
         return self.detail
-
-
-class ValidationError(PortalAuthException):
-    message_format = _("Validation Error.")
-    status_code = 400
-    title = 'Validation Error'
-
-
-class Unauthorized(PortalAuthException):
-    message_format = _("The request you have made requires authentication.")
-    status_code = 401
-    title = 'Unauthorized'
-
-
-class UserNotActivatedException(Unauthorized):
-    # code = CODE.USERNOTACTIVATED_EXCEPTION_CODE
-    message_format = _('User Not Activated')
-    title = 'User Not Activated'
-
-
-class UserDormancyException(Unauthorized):
-    code = CODE.USER_DORMANCY_EXCEPTION_CODE
-    message_format = _('User Dormancy')
-    title = 'User Dormancy'
-
-
-class UserDisabledException(Unauthorized):
-    code = CODE.USERDISABLED_EXCEPTION_CODE
-    message_format = _('User has been frozen')
-
-
-class NotFound(PortalAuthException):
-    message_format = _('Not Found')
-    status_code = 404
-    title = 'Not Found'
-
-
-class UserNotFound(NotFound):
-    code = CODE.USERNOTFOUND_CODE
-    message_format = _('User Not Found')
-
-    def __init__(self, user=None):
-        super(UserNotFound, self).__init__()
-        if user:
-            self.message_format = _("Could not find user: %s") % user
-
-
-class ForbiddenException(PortalAuthException):
-    status_code = 403
-    message_format = _('Forbidden')
-    title = 'Forbidden'
-
-
-class PasswordVerificationError(ForbiddenException):
-    message_format = _("The password length must be less than or equal "
-                       "to %(size)i. The server could not comply with the "
-                       "request because the password is invalid.")
-    code = CODE.USERNOTFOUND_CODE
-
-    def __init__(self, format=None):
-        super(PasswordVerificationError, self).__init__()
-        if format:
-            self.message_format = self.message_format % format
-
-
-class TokenError(ForbiddenException):
-    code = CODE.TOKEN_ERROR_CODE
-    message_format = _('Token invalid')
-
-
-class LinkUnavailableError(ForbiddenException):
-    code = CODE.LINKUNAVAILABLE_ERROR_CODE
-    message_format = _('Link Unavailable')
-
-
-class OperationFrequentlyError(ForbiddenException):
-    code = CODE.OPERATIONFREQUENTLY_ERROR_CODE
-    message_format = _('operation frequently')
-
-
-class UsernameOrPasswordError(ForbiddenException):
-    code = CODE.USERNAMEORPASSWORD_ERROR_CODE
-    message_format = _('The username or password invalid')
-
-
-class PasswordError(ForbiddenException):
-    code = CODE.PASSWORD_ERROR_CODE
-    message_format = _('The password invalid')
-
-
-class MethodNotAllowedError(PortalAuthException):
-    status_code = 405
-    code = CODE.SERVER_ERROR_CODE
-    message_format = _('Method Not Allowed')
-
-
-class TemporaryPasswordException(ForbiddenException):
-    # code = CODE.TEMPORARY_PASSWORD_CODE
-    message_format = _('For the first time login, The password needs to be reset')
-    title = 'User Password Need Reset'
-
-
-class PasswordNeedResetException(ForbiddenException):
-    # code = CODE.PASSWORD_NEED_RESET_CODE
-    message_format = _('The password has not been changed for a long time, The password needs to be reset.')
-    title = 'User Password Need Reset'
-
-
-class PasswordHistoryRepetitionException(ForbiddenException):
-    code = CODE.PASSWORD_HISTORY_REPETITION_CODE
-    message_format = _('The password has been used in the past')
-    title = 'User Password History Repetition'
-
-
-class TheTemporaryUserHasExpiredException(ForbiddenException):
-    code = CODE.THETEMPORARYUSERHAS
-    message_format = _('User expired')
-    title = 'User expired'
--- cloud_log/models/__init__.py
+++ cloud_log/models/__init__.py
@@ -1,1 +1,3 @@
-from clog import *
+# -*- coding: utf-8 -*-
+
+from clog import Clog
